572a2f05436393c7fd495557ea8dc828b94bb641
Adriana <bussoloadriana@gmail.com>
2025-09-13 16:07:08 -0300

    Adição do projeto gerenciar usuarios com novas funções

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,16 +1,61 @@
-const usuarios = ['matheus', 'marcos', 'lucas', 'joao']; // lista usuarios
+/*
+Exemplo: de um objeto:
+{ 
+    primeiraPropriedade: 'Valor',
+    segundaPropriedade: 'Valor'
+}
+
+*/
+      //lista de objetos com propriedades valores:
+     const usuarios = [
+     { 
+       nome: 'Mateus',
+       email: 'mateus@gospel.com'
+     },
+     { 
+       nome: 'Marcos',
+       email: 'mateus@gospel.com'
+
+     },
+      { 
+       nome: 'Lucas',
+       email: 'lucas@gospel.com'
+     },
+     {
+       nome: 'João',
+       email: 'joao@gospel.com'
+
+     },
+
+     ];
 
  // função que retorna a lista de usuarios
-function retornarUsuarios() {
-    return usuarios;
+      function retornarUsuarios() {
+        return usuarios;
 }
- // funçaõ adicionar novo usuário, pegar um novo nome
-function adicionarNovoUsuario(nome) {
-    usuarios.push(nome);
+
+ // funçaõ adicionar novo usuário, pegar um novo nome, esse nome vai receber um novo usuario
+      function adicionarNovoUsuario(usuario) {
+        usuarios.push(usuario); // dentro da lista de usuarios, colque um usuario no final
 }
 
 // para testar essas funções é preciso exportar
-module.exports = {
-    retornarUsuarios,
-    adicionarNovoUsuario  
+      module.exports = {
+      retornarUsuarios,
+      adicionarNovoUsuario  
 }
+
+// para usae a minha função:
+// capturaPropriedadedeUsuario(0, nome);
+// function capturaPropriedadedeUsuario(indiceDoUsuario, propriedade) {
+//   return usuarios.at.(indiceDoUsuario)[propriedade];
+// }
+// se souber qual propriedade que quero:
+//   function capturaPropriedadeNomeDoUsuario(indiceDoUsuario, propriedade) {
+//   return usuarios.at.(indiceDoUsuario).nome;
+// }
+
+/*
+const usuarios = ['matheus', 'marcos', 'lucas', 'joao']; // lista usuarios
+
+*/
--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -5,18 +5,29 @@ const { expect } = require('chai'); // para comparar
 describe('Testar as funções de Gestão de usuários', function () {
     it('validar que posso adicionar um novo nome de usuários na lista', function () {
 
-        // 1. Adicionar um novo nome na lista de usuários, chamar uma função
-        adicionarNovoUsuario('Maria'); 
+        // 1. Adicionar um novo nome na lista de usuários, e chamar uma função
+        //    adicionarNovoUsuario('Maria'); ->>> passa o objeto, dai la no adicionar Usuario tem que mudar para passar
+        //    o usuario
+          adicionarNovoUsuario({
+             nome: 'Maria',
+             email: 'maria@gospel.com'
+          });
 
 
         // 2. Retornar a lista de usuários na caixa lista de usuários, recebe a função de retornar usuários
-        // Se a função de retornarUsuarios tiver entrada, dai tem que por entrada ali nos parenteses, caso não, não precisa
+        // Se a função de retornarUsuarios tiver entrada, dai tem que por entrada ali nos parenteses, 
+        // caso não, não precisa
         const listaDeUsuarios = retornarUsuarios();
 
         // 3. Comparar se o novo nome está no fim da lista de usuários
-        // Espero uma lista, a função at --> Permite que eu possa escolher a posição do item dentro da lista de usuarios 
+        // A função at --> Permite que eu possa escolher a posição do item dentro 
+        // da lista de usuarios 
         // pega o último item dentro desta lista de usuário
-        expect(listaDeUsuarios.at(-1)).to.equal('Maria');
+        // A função: PONTO significa pegar o nome dele, no caso: .nome
+        // se quiser pegar o email: .email
+        // Pegue o nome do objeto que está por ultimo que tem o nome Maria:
+           expect (listaDeUsuarios.at(-1).nome).to.equal('Maria');
+           expect (listaDeUsuarios.at(-1).email).to.equal('maria@gospel.com');
     });
 });
 
